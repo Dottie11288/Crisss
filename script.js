25f33ec226ace24c54e086d161c560bc099c65b7
Dottie11288 <oossama554@gmail.com>
2026-07-24 22:26:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+// ===============================
+// CAMBIO DE PANTALLAS
+// ===============================
+
+const screens = document.querySelectorAll(".screen");
+
+function showScreen(id) {
+    screens.forEach(screen => {
+        screen.classList.remove("active");
+    });
+
+    document.getElementById(id).classList.add("active");
+}
+
 document.getElementById("startBtn").addEventListener("click", () => {
-    alert("Funciona");
+    showScreen("level1");
 });
+// ===============================
+// CUADRÍCULA 4x4
+// ===============================
+
+const grid = document.getElementById("grid");
+
+for (let i = 0; i < 16; i++) {
+
+    const square = document.createElement("div");
+
+    square.className = "square";
+
+    square.dataset.id = i;
+
+    grid.appendChild(square);
+
+}
